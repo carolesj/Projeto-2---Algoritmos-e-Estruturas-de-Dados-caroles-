7eeeb40f584499123aa4081b04ed8ca32d98bdf5
carolesj <caroline.jesuino.silva@usp.br>
2016-12-11 20:31:25 -0200

    Adicionado o número de chamadas de comandos

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 def main () :
-	O = input().split()
 	
 	rubro_negra = arvore()
-	
-	if (O[0] = "1") :
-		rubro_negra.insercao(O[1])
-	elif (O[0] = "2") :
-		rubro_negra.sucessor(O[1])
-	elif (O[0] = "3") :
-		rubro_negra.predecessor(O[1])
-	elif (O = "4") :
-		rubro_negra.maximo()
-	elif (O = "5") :
-		rubro_negra.minimo()
-	elif (O = "6") :
-		rubro_negra.preordem()
-	elif (O = "7") :
-		rubro_negra.emordem()
-	elif (O = "8") :
-		rubro_negra.posordem()
+	numero_operacoes = int(input())
+
+	for i in range(numero_operacoes):
+		O = input().split()
+		
+		if (O[0] == "1") :
+			rubro_negra.insercao(int(O[1]))
+		elif (O[0] == "2") :
+			rubro_negra.sucessor(int(O[1]))
+		elif (O[0] == "3") :
+			rubro_negra.predecessor(int(O[1]))
+		elif (O == "4") :
+			rubro_negra.maximo()
+		elif (O == "5") :
+			rubro_negra.minimo()
+		elif (O == "6") :
+			rubro_negra.preordem()
+		elif (O == "7") :
+			rubro_negra.emordem()
+		elif (O == "8") :
+			rubro_negra.posordem()
